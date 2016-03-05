4488eb9ea0783f6e0b56ba8a455cac7c0599c5b9
Jacob Killpack <intechninja@gmail.com>
2016-03-05 13:46:23 -0700

    Added a lot of code to match what was in the blog code submitted by the professor.

--- routes/til.js
+++ routes/til.js
@@ -1,9 +1,58 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET til listing. */
+var entries = [
+  {slug:"How to pass class", body: "Come to class. Do your homework", created_at: "03/04/2016"},
+  {slug:"How to fail class", body: "Play video games all day", created_at: "03/04/2016"}
+];
+
+/*READ all: GET til listing. */
 router.get('/', function(req, res, next) {
-  res.render('til/index', { title: 'Today I Learned' });
+  res.render('til/index', { title: 'Today I Learned', entries: entries });
+});
+
+/*CREATE entry form: GET /til/new */
+router.get('/new', function(req, res, next) {
+  res.render('til/new', {title: "Create a new entry"});
+});
+
+/*CREATE entry: POST /til/ */
+router.post('/', function(req, res, next) {
+  entries.push(req.body);
+  res.render('til/index', {title: 'Today I Learned', entries: entries });
+});
+
+/* UPDATE entry form: GET /til/1/edit */
+router.get('/:id/edit', function(req, res, next) {
+  res.render('til/update',
+  {
+    title: 'Update an entry',
+    id: req.params.id,
+    entry: entries[req.params.id]
+  });
+});
+
+/*UPDATE entry: POST /til/1 */
+router.post('/:id', function(req, res, next) {
+  entries[req.params.id] = req.body;
+  res.render('til/index',
+  {
+    title: 'Update an entry',
+    entries: entries
+  });
+});
+
+/* DELETE entry: GET /til/1/delete */
+router.get('/:id/delete', function(req, res, next) {
+  var id = req.params.id
+  entries = entries.slice(0,id).concat(entries.slice(id+1, entries.length));
+  res.render('til/index', {title: 'Today I Learned', entries: entries});
+});
+
+/* THIS NEEDS TO BE LAST or /new goes here rather than where it should be */
+/* READ one entry: GET /til/0 */
+router.get('/:id', function(req, res, next) {
+  res.render('til/entry', {title: "An entry", entry: entries[req.params.id]});
 });
 
 module.exports = router;
